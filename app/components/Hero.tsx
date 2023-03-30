bb0b00264ad1735ca380ca879d899acd1a793deb
David <callmelmao@gmail.com>
2023-03-30 17:36:06 +0100

    navbar done

---
+++ app/components/Hero.tsx
@@ -0,0 +1,14 @@
+export default function Hero() {
+  return (
+    <section>
+      <p>Hi, my name is</p>
+      <h1>David Bazashvili.</h1>
+      <p>I build things for the web</p>
+      <p>
+        I'm a software engineer specialising in Front End Developement with
+        extensive knowledge of Back End. At the moment, I'm looking for a new
+        oportunities and I'd be glad to bring my skills into your business.
+      </p>
+    </section>
+  )
+}
